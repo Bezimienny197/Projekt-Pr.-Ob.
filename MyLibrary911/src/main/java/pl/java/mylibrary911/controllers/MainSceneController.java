6a407e474beb61247c12f2eea3e90f35a281e681
Bezimienny197 <osze2012@gmail.com>
2019-01-07 15:35:45 +0100

    Start project

---
+++ MyLibrary911/src/main/java/pl/java/mylibrary911/controllers/MainSceneController.java
@@ -0,0 +1,13 @@
+package pl.java.mylibrary911.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MainSceneController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+}
